90fcaa8a4a0e811f4bcae44a92fa5ee11bd48edb
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2022-11-09 11:27:24 +0100

    relative path support for dir_listing

--- src/core/Connection.cpp
+++ src/core/Connection.cpp
@@ -2,6 +2,7 @@
 
 #include "../utils/addr_to_str.hpp"
 #include "../utils/color.hpp"
+#include "../utils/get_cwd.hpp"
 #include "../utils/num_to_str.hpp"
 #include "Webserver.hpp"
 
@@ -99,7 +100,8 @@ void Connection::build_response(EventNotificationInterface& eni) {
         try {
             _response.build(_request);
             if (_response.is_dir_listing())
-                _cgi_handler.execute(eni, DIR_LISTING_CGI_PATH, DIR_LISTING_CGI_SCRIPT_PATH);
+                _cgi_handler.execute(eni, DIR_LISTING_CGI_PATH,
+                                     utils::get_absolute_path(DIR_LISTING_CGI_SCRIPT_PATH));
             else if (_response.need_cgi())
                 _cgi_handler.execute(eni, _response.cgi_pass()->path, _request.relative_path());
         } catch (int error) {
