f00938cdb186344ad365a8889a30c74b8b042c02
Kevin Hirsig <78398628+khirsig@users.noreply.github.com>
2022-11-10 13:47:14 +0100

    dir listing updated

--- cgi/directory_listing.py
+++ cgi/directory_listing.py
@@ -2,13 +2,26 @@
 
 import os
 from datetime import datetime, timezone
-# import sys
 import cgi
 import cgitb
+
 cgitb.enable()
 
+
+def human_readable_size(size, decimal_places=2):
+    for unit in ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB']:
+        if size < 1024.0 or unit == 'PiB':
+            break
+        size /= 1024.0
+    return f"{size:.{decimal_places}f} {unit}"
+
+
 form = cgi.FieldStorage()
-form.getvalue('script_filename')
+dir = "."
+dir_suffix = os.environ['SCRIPT_FILENAME']
+if (dir_suffix != None):
+    dir += "/" + dir_suffix
+
 
 print("Content-Type: text/html\r\n")
 
@@ -19,13 +32,25 @@ print("</head>")
 print("<body>")
 print("<h2>Directory Listing</h2>")
 print("<table border='2'>")
-print("<tr><th>Name</th><th>Size</th><th>Last Modified</th></tr>")
-for file in os.listdir('.'):
+print("<tr><th>Type</th><th>Name</th><th>Size</th><th>Last Modified</th></tr>")
+if (dir != "./"):
+    print("<tr><td>Directory</td><td><a href='..'>..</a></td><td></td><td></td></tr>")
+for file in os.listdir(dir):
+    full_path = dir + file
     print("<tr>")
+    if (os.path.isdir(full_path)):
+        dir_string = "Directory"
+    elif ((os.path.islink(full_path))):
+        dir_string = "Link"
+    elif ((os.path.isfile(full_path))):
+        dir_string = "File"
+    else:
+        dir_string = "Unknown"
+    print("<td>%s</td>" % dir_string)
     print("<td><a href='%s'>%s</a></td>" % (file, file))
-    print("<td>%s</td>" % os.path.getsize(file))
+    print("<td>%s</td>" % human_readable_size(os.path.getsize(full_path)))
     print("<td>%s</td>" %
-          datetime.fromtimestamp(os.path.getmtime(file), timezone.utc))
+          datetime.fromtimestamp(os.path.getmtime(full_path), timezone.utc))
     print("</tr>")
 print("</table>")
 print("</body>")
