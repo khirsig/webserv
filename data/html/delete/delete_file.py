e0807d85577a5526f0951d68c72521c4e1b9900c
Kevin Hirsig <78398628+khirsig@users.noreply.github.com>
2022-11-10 16:57:40 +0100

    delete script

---
+++ data/html/delete/delete_file.py
@@ -0,0 +1,36 @@
+# Script that deletes the file at the given path by html form
+
+import cgi
+import cgitb
+import os
+
+cgitb.enable()
+
+form = cgi.FieldStorage()
+
+method = os.environ.get('REQUEST_METHOD')
+
+if method != 'DELETE' and method != 'POST':
+    message = 'Only DELETE and POST method is supported'
+else:
+    file_item = form['filename'].value
+    # Test if the file was uploaded
+    if file_item:
+        if (file_item == "delete_file.py"):
+            message = 'Can not delete itself'
+        else:
+            fn = os.path.basename(file_item.replace("\"", " /"))
+            if (os.path.isfile(fn)):
+                os.remove(fn)
+                message = 'The file "' + file_item + '" was deleted successfully'
+            else:
+                message = 'The file "' + file_item + '" does not exist'
+    else:
+        message = 'No file was deleted'
+
+print("Content-Type: text/html\r\n\r\n", end='')
+print("<html>")
+print("<body>")
+print("<h3>" + message + "</h3>")
+print("</body>")
+print("</html>")
