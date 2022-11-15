b4ba0963413be48f8cbda0552926fec11c37d627
Kevin Hirsig <78398628+khirsig@users.noreply.github.com>
2022-11-15 15:45:33 +0100

    deleted unecessary files

--- data/html/test.py
+++ data/html/test.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python
 
-# # Import modules for CGI handling
-# import cgi
-# import cgitb
-
-# # Create instance of FieldStorage
-
-# # Get data from fields
-# first_name = form.getvalue('first_name')
-# last_name = form.getvalue('last_name')
-
 print("Content-Type: text/html\r\n\r\n", end = '')
 
 import os
